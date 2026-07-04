847d08d21a67a4d9a07925f09ad2cf2773aca147
Vinit Kumar <vinitgoswami68@gmail.com>
2026-07-04 19:43:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,18 @@ function sendMessage() {
     }, 500);
 
 }
+.user-message{
+    background:#0078ff;
+    color:white;
+    padding:10px;
+    border-radius:10px;
+    width:fit-content;
+    margin-left:auto;
+    margin-bottom:10px;
+    max-width:80%;
+}
+
+.bot-message{
+    margin-bottom:10px;
+    max-width:80%;
+}
